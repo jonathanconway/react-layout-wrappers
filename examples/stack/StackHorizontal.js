b11d45d9c83e70a2ef91310b14df25f801c9e28a
Jonathan <jonathan.conway@gmail.com>
2018-12-23 14:12:37 +0000

    Fix Stack to expand children to take up full width/height

--- examples/stack/StackHorizontal.js
+++ examples/stack/StackHorizontal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Stack } from 'react-layout';
 
 const StackHorizontal = () => (
-  <Stack orientation="horizontal">
+  <Stack orientation="horizontal" style={{ height: '50px' }}>
     <span>Text</span>
     <button>Button 1</button>
     <button>Button 2</button>
--- src/Stack.tsx
+++ src/Stack.tsx
@@ -6,15 +6,28 @@ import { createFlexDirectionCalculator } from './utils';
 
 export interface StackProps extends WrapperProps, SequenceWrapperProps {}
 
-const calculateFlexDirection = createFlexDirectionCalculator({
+const defaultProps: StackProps = {
   orientation: 'vertical',
   direction: 'left-to-right',
-});
+};
+
+const calculateFlexDirection = createFlexDirectionCalculator(defaultProps);
+
+const calculateWidthOrHeightSetting = (props: StackProps) =>
+  ({
+    horizontal: 'height: 100%;',
+    vertical: 'width: 100%;',
+  }[props.orientation || defaultProps.orientation!]);
 
 const StackDiv = styled.div`
   display: flex;
   align-items: center;
   flex-direction: ${calculateFlexDirection};
+  vertical-align: top;
+
+  & > * {
+    ${calculateWidthOrHeightSetting};
+  }
 `;
 
 /**
