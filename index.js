4132d39579e4b98bd3db1a7e440ca3b305123746
Jonathan <jonathan.conway@gmail.com>
2018-12-29 18:30:26 +0000

    Fix export module name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./react-layout.cjs.production.js');
+  module.exports = require('./react-layout-wrappers.cjs.production.js');
 } else {
-  module.exports = require('./react-layout.cjs.development.js');
+  module.exports = require('./react-layout-wrappers.cjs.development.js');
 }
